c8e6414ef4c1aaf2f47713bb87272734c4fcf3f9
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-03-18 19:54:36 +0200

    fixing es6 issue

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.get("/api/test", (req, res) => {
-  console.log("Hi Guys");
   res.send({ message: "We are just testing" }).status(200);
 });
 app.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
 });
+
