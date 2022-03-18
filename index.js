e4fc38450d514288caf6e5b6a186bbb4c2168bea
YossefMohamed <youssef171035@feng.bu.edu.eg>
2022-03-18 16:05:38 +0200

    fixing es6 issue

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.get("/api/test", (req, res) => {
+  console.log("Hi Guys");
+  res.send({ message: "We are just testing" }).status(200);
+});
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
+});
---
+++ src/database.js
@@ -0,0 +1,10 @@
+const Users = [
+  {
+    id: 1,
+    email: "test@test.com",
+    name: "Tester",
+    password: "123456789",
+  },
+];
+
+module.exports = Users;
