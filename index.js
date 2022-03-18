a8b1beae3fc080d17e776ecf8989d208f385190c
Yasser Magdy <47793179+YasserElmenshawy@users.noreply.github.com>
2022-03-18 19:50:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const getUser = require("./src/controllers/userControllers/getUser");
 
 const app = express();
 
