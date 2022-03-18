5677f07b6f8c8cdf8bea81e7edf53ca48ef555a4
Ahmed Hesham Salah <42468050+AhmedHeshams@users.noreply.github.com>
2022-03-18 17:30:38 +0200

    add getAllUsers get req

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
+const getAllUsers = require("./src/controllers/userControllers/getAllUsers");
 const app = express();
 
 app.use(morgan("dev"));
@@ -11,6 +11,7 @@ app.get("/api/test", (req, res) => {
   console.log("Hi Guys");
   res.send({ message: "We are just testing" }).status(200);
 });
+app.get("/api/users", getAllUsers);
 app.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
 });
