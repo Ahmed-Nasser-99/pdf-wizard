ecdbfa03c24760fa6da2ee17dfad9c80e37a03f3
Ahmed Nasser <43070604+Ahmed-Nasser-99@users.noreply.github.com>
2022-04-15 01:33:15 +0200

    removing some duplicated lines.

--- index.js
+++ index.js
@@ -1,61 +1,23 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
-
 const deleteUser = require("./src/controllers/userControllers/deleteUser");
-
 const addUser = require("./src/controllers/userControllers/addUser");
-
-
 const getAllUsers = require("./src/controllers/userControllers/getAllUsers");
-
-const addUser = require("./src/controllers/userControllers/addUser");
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const addUser = require('./src/controllers/userControllers/addUser');
-const updateUser = require('./src/controllers/userControllers/updateUser');
+const updateUser = require("./src/controllers/userControllers/updateUser");
 const getUser = require("./src/controllers/userControllers/getUser");
-
-
-
-
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.delete("/api/users/:id",deleteUser)
-
-
-
-app.get("/api/test", (req, res) => {
-  res.send({ message: "We are just testing" }).status(200);
-
-
-
-// get
+app.delete("/api/users/:id", deleteUser);
 app.get("/api/users/:id", getUser);
-app.get('/api/test', (req, res) => {
-  res.send({ message: 'We are just testing' }).status(200);
-
-});
-
 app.get("/api/users", getAllUsers);
-
-
-// post
-
-app.post('/api/users', addUser);
-
-// put
-app.put('/api/users/:id', updateUser);
-
-// delete
+app.post("/api/users", addUser);
+app.put("/api/users/:id", updateUser);
 
 app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
+  console.log("Server is running on http://localhost:8080");
 });
