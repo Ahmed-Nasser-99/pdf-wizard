bc2267530e6833f4dcfe3627aa2e05a4cb4477ed
Ahmed-Nasser-99 <forall3020@gmail.com>
2022-04-18 16:21:49 +0200

    linking with mongoDB and creating the user schema

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
+const dotenv = require("dotenv");
 const deleteUser = require("./src/controllers/userControllers/deleteUser");
 const addUser = require("./src/controllers/userControllers/addUser");
 const getAllUsers = require("./src/controllers/userControllers/getAllUsers");
 const updateUser = require("./src/controllers/userControllers/updateUser");
 const getUser = require("./src/controllers/userControllers/getUser");
+const connectDB = require("./src/database");
+
+dotenv.config({ path: "./.env" });
+connectDB();
 
 const app = express();
 
--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
-const Users = [
-  {
-    id: 1,
-    email: "test@test.com",
-    name: "Tester",
-    password: "123456789",
-  },
-];
+const mongoose = require("mongoose");
 
-module.exports = Users;
+const connectDB = async () => {
+  mongoose
+    .connect(
+      `mongodb+srv://ahmed:${process.env.MONGODB_PASSWORD}@cluster0.azwhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    )
+    .then(() => console.log("Connected to mongoDB"))
+    .catch((e) => console.log(e.message));
+};
+
+module.exports = connectDB;
