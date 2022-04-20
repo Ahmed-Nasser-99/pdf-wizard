6ad780cebd79e0a7f18ab3b454659c167fee7434
AhmedY0unes <Ahm3dy0un3s@gmail.com>
2022-04-20 23:10:24 +0200

    add packages

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = multer({
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (ext !== '.pdf' && ext !== '.word' && ext !== '.pptx') {
+    if (ext !== '.pdf' && ext !== '.docx' && ext !== '.pptx') {
       cb(new Error('File type is not supported'), false);
       return;
     }
