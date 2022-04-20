0ba86e24ea56d9b911698abc79ce8cc2252f8202
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-04-21 00:27:23 +0200

    user protect middleware

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const updateUser = require("./src/controllers/userControllers/updateUser");
 const getUser = require("./src/controllers/userControllers/getUser");
 const postFile = require("./src/controllers/fileControllers/postFile");
 const connectDB = require("./src/database");
+const { protect } = require("./src/utils/authGard");
 
 dotenv.config({ path: "./.env" });
 connectDB();
@@ -17,12 +18,15 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.delete("/api/users/:id", deleteUser);
+
 app.get("/api/users/:id", getUser);
 app.get("/api/users", getAllUsers);
 app.post("/api/users", addUser);
-app.put("/api/users/:id", updateUser);
 
+app.use(protect)
+
+app.delete("/api/users/:id", deleteUser);
+app.put("/api/users/:id", updateUser);
 app.post("/api/files", postFile);
 
 app.listen(8080, () => {
