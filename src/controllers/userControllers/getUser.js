93b57b86d7519de77172efa15216fdf921f92f9f
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-04-21 00:22:02 +0200

    Add Auth Middleware

--- src/controllers/userControllers/getUser.js
+++ src/controllers/userControllers/getUser.js
@@ -1,8 +1,8 @@
 const User = require("../../models/userModel");
 
-const getUser = await (req, res) => {
-  const {id} =req.params;
-  const user = await User.findById(id)
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
   if (user) {
     res.json({
       status: "ok",
