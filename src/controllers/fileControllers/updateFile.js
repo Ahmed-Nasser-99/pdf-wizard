90651cffc838e8a97962e32a9b7ac0b1c8bf67b6
Yasser Magdy <47793179+YasserElmenshawy@users.noreply.github.com>
2022-04-21 13:47:02 +0200

    finish updateFile controller

---
+++ src/controllers/fileControllers/updateFile.js
@@ -0,0 +1,31 @@
+const Pdf = require("../../models/pdfModel");
+async function updateFile(req, res) {
+  const { name , description } = req.body;
+  try{
+   
+    const file = await Pdf.findOne({
+      userId: req.user.id,
+      fileId: req.params.id
+    });
+   
+    if(file){
+      file.name = name
+      file.description = description
+      await file.save()
+      
+      res.status(200).json({
+        status: "ok",
+        data: file
+        })
+      }
+    else{
+      res.status(404).json({
+        status: "File Not Found"
+        })
+    }
+  } catch(err){
+    console.log(err);
+  }
+}
+
+module.exports = updateFile;
