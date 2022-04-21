03681619c3f4e01befb5557e494a026cd7a75faf
Ahmed-Nasser-99 <forall3020@gmail.com>
2022-04-21 17:03:07 +0200

    adding the file size limit and pdf extenstion limit

--- src/controllers/fileControllers/postFile.js
+++ src/controllers/fileControllers/postFile.js
@@ -3,13 +3,14 @@ const Pdf = require("./../../models/pdfModel");
 
 async function postFile(req, res) {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      format: "pdf",
-    });
+    const ext = req.file.originalname.slice(req.file.originalname.indexOf(".")+1)
+    if(ext !== "pdf")
+      return res.status(400).send({message: 'File extension is not supported (.pdf only). Use convert instead'});
+    const result = await cloudinary.uploader.upload(req.file.path);
     const fileData = {
       name: req.file.originalname,
       link: result.secure_url,
-      userId: req.userId,
+      userId: req.user._id,
       description: req.body.description,
       fileId: result.public_id,
     };
