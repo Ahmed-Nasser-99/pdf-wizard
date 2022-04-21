5de090195ea0548ba3679ab46dfc59c8be2356e2
unknown <ahmed.hesham_1999@yahoo.com>
2022-04-21 04:03:36 +0200

    add getFile route

---
+++ src/controllers/fileControllers/getFile.js
@@ -0,0 +1,25 @@
+const Pdf = require("../../models/pdfModel");
+async function getFile(req, res) {
+  try{
+   
+    const file = await Pdf.findOne({
+      userId: req.user.id,
+      fileId: req.params.id
+    });
+    if(file){
+      res.status(200).json({
+        status: "ok",
+        data: file
+        })
+      }
+    else{
+      res.status(404).json({
+        status: "No file exist with this id"
+        })
+    }
+  } catch(err){
+    console.log(err);
+  }
+}
+
+module.exports = getFile;
