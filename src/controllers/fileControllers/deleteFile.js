132de3b057727f31fb57312b9c70a22a96bfd6b5
Yossef mohamed <47724276+YossefMohamed@users.noreply.github.com>
2022-04-23 13:49:27 +0200

    fix deleteFile issue

--- src/controllers/fileControllers/deleteFile.js
+++ src/controllers/fileControllers/deleteFile.js
@@ -4,9 +4,13 @@ const Pdf = require("../../models/pdfModel");
 async function deleteFile(req,res){
   try{
      const file = await Pdf.findOne({
-         userId: req.user.id,
          _id: req.params.id
        });
+      if(file.userId.equals(req.user.id)){
+         return res.status(404).json({
+            status: "no file exist with this id to be deleted",
+          });  
+      }
        if(file){
         await file.delete();
         res.status(204).json({
