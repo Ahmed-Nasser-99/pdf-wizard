2aef32e66f2d77b6e664a8b22d524c575ab76c9a
AhmedY0unes <Ahm3dy0un3s@gmail.com>
2022-04-23 22:32:57 +0200

    deleteFile now deletes files from cloudinary

--- src/controllers/fileControllers/deleteFile.js
+++ src/controllers/fileControllers/deleteFile.js
@@ -1,17 +1,18 @@
-const cloudinary = require("../../cloudinary");
-const Pdf = require("../../models/pdfModel");
+const cloudinary = require('../../cloudinary');
+const Pdf = require('../../models/pdfModel');
 
 async function deleteFile(req, res) {
   try {
     const file = await Pdf.findById(req.params.id);
     if (file && file.userId.equals(req.user._id)) {
+      const result = await cloudinary.uploader.destroy(file.fileId); // delete file on cloudinary
       await file.delete();
       res.status(204).json({
-        status: "ok",
+        status: 'ok',
       });
     } else {
       res.status(404).json({
-        status: "no file exist with this id to be deleted",
+        status: 'no file exist with this id to be deleted',
       });
     }
   } catch (err) {
