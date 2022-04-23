6f3986aa28ae7d061d9be4352e86a3f350b40368
Ahmed-Nasser-99 <forall3020@gmail.com>
2022-04-23 03:59:56 +0200

    frontend touches

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 import Header from "./components/Header";
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,21 @@ export default function Header() {
   const [isUser, setUser] = React.useState(
     localStorage.getItem("userId") ? true : false
   );
+  const [navBarToggled, setNavBarToggled] = React.useState(false);
   const navigate = useNavigate();
+  const toggleNavbar = () => {
+    setNavBarToggled(!navBarToggled);
+
+    if (navBarToggled === true) {
+      setTimeout(() => {
+        const togglerButton = document.getElementById("navBarToggler");
+        togglerButton!.setAttribute("aria-expanded", "false");
+        const togglerDiv = document.getElementById("navbarSupportedContent");
+        togglerDiv!.className = "navbar-collapse collapse";
+        clearTimeout();
+      }, 500);
+    }
+  };
 
   useEffect(() => {
     localStorage.getItem("userId") ? setUser(true) : setUser(false);
@@ -13,71 +27,75 @@ export default function Header() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-5 border ">
-      <Link className="navbar-brand" to="/">
-        PDF Wizard
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link " to="/">
-              Home{" "}
-            </Link>
-          </li>
-          <li className="nav-item">
-            {!isUser && (
-              <Link className="nav-link " to="/login">
-                login
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          PDF Wizard
+        </Link>
+        <button
+          aria-expanded="false"
+          className="navbar-toggler"
+          onClick={toggleNavbar}
+          id="navBarToggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link " to="/">
+                Home{" "}
               </Link>
-            )}
-          </li>
-          <li className="nav-item">
-            {!isUser && (
-              <Link className="nav-link " to="/register">
-                register
-              </Link>
-            )}
-          </li>
-          <li className="nav-item">
-            {isUser && (
-              <Link className="nav-link " to="/myfiles">
-                My Lib
-              </Link>
-            )}
-          </li>{" "}
-          <li className="nav-item">
-            {isUser && (
-              <Link className="nav-link " to="/profile">
-                My Profile
-              </Link>
-            )}
-          </li>{" "}
-          <li className="nav-item">
-            {isUser && (
-              <Link
-                className="nav-link ml-auto"
-                to="#"
-                onClick={() => {
-                  localStorage.removeItem("token");
-                  localStorage.removeItem("userId");
-                  window.location.reload();
-                }}
-              >
-                Logout
-              </Link>
-            )}
-          </li>
-        </ul>
+            </li>
+            <li className="nav-item">
+              {!isUser && (
+                <Link className="nav-link " to="/login">
+                  login
+                </Link>
+              )}
+            </li>
+            <li className="nav-item">
+              {!isUser && (
+                <Link className="nav-link " to="/register">
+                  register
+                </Link>
+              )}
+            </li>
+            <li className="nav-item">
+              {isUser && (
+                <Link className="nav-link " to="/myfiles">
+                  My Lib
+                </Link>
+              )}
+            </li>{" "}
+            <li className="nav-item">
+              {isUser && (
+                <Link className="nav-link " to="/profile">
+                  My Profile
+                </Link>
+              )}
+            </li>{" "}
+            <li className="nav-item">
+              {isUser && (
+                <Link
+                  className="nav-link ml-auto"
+                  to="#"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("userId");
+                    window.location.reload();
+                  }}
+                >
+                  Logout
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
